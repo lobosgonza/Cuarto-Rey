043c8a32b1833e226c4c4619f75b6ad8175fc4e3
unknown <lobosgonza60@gmail.com>
2020-10-03 17:57:00 -0300

    Desarrollo de mazo de cartas

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,51 @@
+let cartasUtilizadas = [];
+const pintasSet = ["Pica", "Trebol", "Corazon", "Diamante"]
+
+
 function randomCard(){
-    document.getElementById('card').innerHTML = Math.ceil(Math.random() * 13);
+    const randomNumber = Math.ceil(Math.random() * 13);
+    const randomPinta = Math.floor(Math.random() * 4);
+
+
+    document.getElementById('number').innerHTML ="Numero: " + randomNumber;
+    document.getElementById('pinta').innerHTML = "Pinta: " + pintasSet[randomPinta];
+
+
+
+    const cartaActiva = randomNumber + " " + pintasSet[randomPinta];
+
+
+
+    // Antes debe evaluarse si dentro del array cartaActiva no esta repetido mas de 4 veces. Si cartaActiva está reeptido 1 o mas veces, el sistema debe volver a lanzar una carta Activa
+
+const filtroCartaActiva = cartasUtilizadas.filter(
+    (value)=>{
+        return value === cartaActiva
+    }
+    );
+
+const longitudCartaActiva = filtroCartaActiva.length;
+console.log(cartaActiva);
+
+if (cartasUtilizadas.length < 52){
+    pushCard()
+}else{
+    alert("no quedan mas cartas")
+}
+
+function pushCard(){
+if (longitudCartaActiva >= 1){ 
+   randomCard()
+} else {
+    cartasUtilizadas.push(cartaActiva)
+}
+}
+
+
+ 
+    
+    
+    
+    console.log(cartasUtilizadas.sort());
+return cartasUtilizadas;
 }
