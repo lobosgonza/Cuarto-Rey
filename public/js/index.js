010e888710042301a51fb45644e7d3f22cd40fb4
unknown <lobosgonza60@gmail.com>
2020-10-04 09:54:31 -0300

    arreglos bulma y desarrollo boton refresh

--- public/js/index.js
+++ public/js/index.js
@@ -1,8 +1,11 @@
-let cartasUtilizadas = [];
+const cartasUtilizadas = [];
 const pintasSet = ["Pica", "Trebol", "Corazon", "Diamante"]
+const reyes = []
 
 
 function randomCard(){
+
+    // Generador de numeros + pinta
     const randomNumber = Math.ceil(Math.random() * 13);
     const randomPinta = Math.floor(Math.random() * 4);
 
@@ -14,9 +17,13 @@ function randomCard(){
 
     const cartaActiva = randomNumber + " " + pintasSet[randomPinta];
 
+  
+
+   
+  
 
 
-    // Antes debe evaluarse si dentro del array cartaActiva no esta repetido mas de 4 veces. Si cartaActiva está reeptido 1 o mas veces, el sistema debe volver a lanzar una carta Activa
+    // Antes debe evaluarse si dentro del array cartaActiva no esta repetido mas de 4 veces. Si cartaActiva está reptido 1 o mas veces, el sistema debe volver a lanzar una carta Activa
 
 const filtroCartaActiva = cartasUtilizadas.filter(
     (value)=>{
@@ -27,25 +34,34 @@ const filtroCartaActiva = cartasUtilizadas.filter(
 const longitudCartaActiva = filtroCartaActiva.length;
 console.log(cartaActiva);
 
-if (cartasUtilizadas.length < 52){
-    pushCard()
+
+// Condicional de Finalizacion de Juego
+if (cartasUtilizadas.length >= 52 || reyes.length > 4 ){
+
+
 }else{
-    alert("no quedan mas cartas")
+    pushCard()
 }
 
+
+// La carta va al array
 function pushCard(){
 if (longitudCartaActiva >= 1){ 
    randomCard()
 } else {
     cartasUtilizadas.push(cartaActiva)
-}
+};
+
+//Reyes van a array Reyes
+if (randomNumber === 13){
+    reyes.push(cartaActiva); 
+    console.log(reyes)
+};
 }
 
 
- 
-    
     
     
     console.log(cartasUtilizadas.sort());
-return cartasUtilizadas;
+
 }
