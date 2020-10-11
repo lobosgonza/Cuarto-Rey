b5f6c2c626fc242891aac3b7f276f78619da84c8
unknown <lobosgonza60@gmail.com>
2020-10-11 01:21:29 -0300

    arreglo archivos

--- public/js/index.js
+++ public/js/index.js
@@ -8,11 +8,11 @@ const reyes = []
 
 
 function randomCard(){
-
+    
     // Generador de numeros + pinta
     const randomNumber = Math.ceil(Math.random() * 13);
     const randomPinta = Math.floor(Math.random() * 4);
-
+    
 
     document.getElementById('number').innerHTML ="Numero: " + randomNumber;
     document.getElementById('pinta').innerHTML = "Pinta: " + pintasSet[randomPinta];
@@ -71,5 +71,15 @@ if (randomNumber === 13){
     
     
     console.log(cartasUtilizadas.sort());
+
+
+
+//agregar titulo al respectivo turno
+    const jugadores = document.getElementById("playerTurn").innerHTML; //array de jugadores
+    console.log(jugadores);
+    const turnDiv = document.getElementById("turnDisplay");
+    turnDiv.innerText = "Turno de " + document.getElementById("1").innerText; //indica lo que hay en elClick
+
+
 
 }
