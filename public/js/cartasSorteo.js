4bd8d09d28e4794cb6b7528998b19d321639588d
unknown <lobosgonza60@gmail.com>
2020-10-15 19:02:50 -0300

    arreglo responsivo y cambio de colores. Refactoring y mejor funcionalidad de sorteo de cartas

--- public/js/cartasSorteo.js
+++ public/js/cartasSorteo.js
@@ -1,9 +1,7 @@
 //jshint esversion:6
-const cartas = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
 
-const randomCard = Math.floor(Math.random()*3);
-console.log(randomCard);
-console.log(cards);
+const randomCard = Math.floor(Math.random() * 3);
+
 
 
 
@@ -13,62 +11,68 @@ let juegoEmpezado = false;
 //   return this[Math.floor(Math.random()*this.length)];
 // }                                                           <== randomCard hace la misma wea
 
-  //////////////////////////////
- ///// Lista de cartas ////////
 //////////////////////////////
-const cards = [
-    {
-      source:"./img/pinochin.jpeg", 
-  title:"Pinochin de copas",
-  instructions:"Piscolits al seco perri3"
-},{
-    source:"https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.ytimg.com%2Fvi%2FZi3bKtkrdfs%2Fmaxresdefault.jpg&f=1&nofb=1",
-  title:"EL Barney del 7",
-  instructions:"juego del 7"
-},{
-    source: "https://www.eldesconcierto.cl/wp-content/uploads/2016/03/udi.jpg",
-      title:"Jaimito",
-  instructions:"Todos toman con la mano derecha hasta el siguiente Jaimito"
-}
-];
+///// Lista de cartas ////////
+//////////////////////////////
 
+
+const cards = [{
+  id: 0,
+  key: 0,
+  source: "./img/pinochin.jpeg",
+  title: "Yisus multiplica el copete",
+  instructions: "Todos al Seco"
+}, {
+  id: 1,
+  key: 1,
+  source: "https://i.kym-cdn.com/photos/images/newsfeed/000/128/847/400x.jpg",
+  title: "El Pana Greg",
+  instructions: "Regala Uno"
+}, {
+  id: 2,
+  key: 2,
+  source: "https://www.infogate.cl/wp-content/uploads/2016/09/Nachito-Pop-Rejugados.jpg",
+  title: "Nachito Pop",
+  instructions: "Le mas bonite toma"
+}, {
+  id: 3,
+  key: 3,
+  source: "https://www.eldesconcierto.cl/wp-content/uploads/2020/08/A_UNO_1204029_3f6de.jpg",
+  title: "El Pizzas",
+  instructions: "El mas ladrón toma."
+}, {
+  id: 4,
+  key: 4,
+  source: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.ytimg.com%2Fvi%2FZi3bKtkrdfs%2Fmaxresdefault.jpg&f=1&nofb=1",
+  title: "El Barney del 7",
+  instructions: "juego del 7"
+}]
+
+
+console.log(cards);
+
+// crear objeto carta
+
+Array.prototype.sample = function () {
+  return this[Math.floor(Math.random() * this.length)];
+}
 
 
 // crear objeto carta
 
-Array.prototype.sample = function(){
-  return this[Math.floor(Math.random()*this.length)];
-}
 
-const cards=[
-  {source:"./img/pinochin.jpeg", title:"Pinochin de copas",
-  instructions:"Piscolits al seco perri3"},
-  {source:"https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.ytimg.com%2Fvi%2FZi3bKtkrdfs%2Fmaxresdefault.jpg&f=1&nofb=1",
-  title:"uwu",
-  instructions:"regala uno"},
-  {source:"https://www.infogate.cl/wp-content/uploads/2016/09/Nachito-Pop-Rejugados.jpg",
-  title:"Nachito Pop",
-  instructions:"El mas weon toma"},
-  {source:"https://www.eldesconcierto.cl/wp-content/uploads/2020/08/A_UNO_1204029_3f6de.jpg",
-  title:"El lanza",
-  instructions:"el mas ladron toma, si son todos unos fomes culiaos el mas cagao."}
-]
+function newCard() { // TODO Cambiar por funcion que reciba objeto carta y ponga wea en la wea
 
+  if (juegoEmpezado === false) {
 
-// crear objeto carta
+    // Image sample
+    // const carta = cards[randomCard];
+    carta = cards.sample();   // => Elige un element random
+    console.log(carta);
 
-
-function newCard() {// TODO Cambiar por funcion que reciba objeto carta y ponga wea en la wea
-
-if (juegoEmpezado === false){
-
-// Image sample
-// const carta = cards[randomCard];
-    carta = cards.sample()
-
-/////////////////////////
+    /////////////////////////
     //  Nueva imagen
-//////////////////////////
+    //////////////////////////
     /*
     const newImg = document.createElement("img");
     const img = document.getElementById("cardI");
@@ -78,7 +82,7 @@ if (juegoEmpezado === false){
     src.value = carta.source
     document.images.namedItem("CardImg").src = carta.source;
     */
-    document.images[1].src=carta.source
+    // document.images[1].src = carta.source  
 
 
     /*
@@ -86,9 +90,9 @@ if (juegoEmpezado === false){
     newImg.setAttributeNode(imgAtt);
     img.setAttributeNode(src);                       // crea imagen con sus clases correspondientes
     */
-/////////////////////////
+    /////////////////////////
     // Nuevo Texto
-/////////////////////////
+    /////////////////////////
     /*
     const newText = document.createElement("h2");
     newText.innerText = carta.title
@@ -106,27 +110,28 @@ if (juegoEmpezado === false){
     // crea el texto correpondiente con sus atributos
 
     */
-    document.getElementById("CardTitle").innerText=carta.title
-    document.getElementById("CardIns").innerText=carta.instructions
+   document.getElementById("CardImg").src = carta.source;
+    document.getElementById("CardTitle").innerText = carta.title;
+    document.getElementById("CardIns").innerText = carta.instructions;
 
 
 
 
-    const buttonStart = document.getElementById("startBtn");//[?] porque tiene que estr esto aca?
+    const buttonStart = document.getElementById("startBtn"); //Apunta al boton de iniciar y luego cambia el nombre del boton con Inner Text
     buttonStart.innerText = "Recoge otra carta";
-/////////////////////////
+    /////////////////////////
     // crear Div carta
-/////////////////////////
-const cardDiv = document.getElementById("card");
-const cardAtt = document.createAttribute("class");
-cardAtt.value = "card";
-cardDiv.setAttributeNode(cardAtt);                      //crea el div Card que contiene la imagen y el texto de la carta
+    /////////////////////////
+    const cardDiv = document.getElementById("card");
+    const cardAtt = document.createAttribute("class");
+    cardAtt.value = "card";
+    cardDiv.setAttributeNode(cardAtt); //crea el div Card que contiene la imagen y el texto de la carta
 
 
-/////////////////////////
-// Toggle juegoEmpezado
-/////////////////////////
+    /////////////////////////
+    // Toggle juegoEmpezado
+    /////////////////////////
     juegoEmpezado = true;
-}
+  }
 
 }
